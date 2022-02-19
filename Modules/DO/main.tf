105f8822ccf7a835e887617cdb6db6ce3ea4e47c
Prabesh Thapa <sthapaprabesh2020@gmail.com>
2022-02-19 19:28:03 +1100

    Addded configuration to create VM in digital ocean

--- Modules/DO/main.tf
+++ Modules/DO/main.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_droplet" "test_vm" {
   image  = "ubuntu-18-04-x64"
-  name   = "Terraform built VM"
+  name   = "terraform-vm"
   region = "sgp1"
   size   = "s-1vcpu-1gb"
 }
